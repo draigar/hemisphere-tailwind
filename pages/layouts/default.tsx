d92b7f71b509beb900cd626e99dabb32e0bb213c
Ben Francis <siscorule@gmail.com>
2022-02-05 11:28:55 +0100

    Added some updates

---
+++ pages/layouts/default.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const isServer = typeof window === 'undefined'
+const WOW = !isServer ? require('wow.js') : null
+
+interface DefaultLayoutProps {
+    // navContent: navTypes;
+    children?: any;
+}
+
+export const DefaultLayout = (props: DefaultLayoutProps) => {
+    const { children } = props;
+    // const isFetching = useIsFetching()
+
+    React.useEffect(() => {
+        new WOW().init()
+    },[])
+    return null
+}
