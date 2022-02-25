b3299bcf6ac0b88e29288d73585a2d61729f9949
soudengwu <soudengwu@naurix.com>
2022-02-25 11:55:10 +0100

    fix:carousel

--- src/components/carousel/carousel3.tsx
+++ src/components/carousel/carousel3.tsx
@@ -6,12 +6,15 @@ import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { utilities } from "@web/helpers/utilities";
+import CustomText from "../text/customText";
 
-const Carousel3 = ({ content}: any) => {
+const Carousel3 = (props: props) => {
+    const { content } = props;
     const heading = content?.heading?.text;
     const caption = content?.captionText?.text;
     const titleText = content?.titleText?.text;
-    const cards = content?.cards;
+    const carouselElements = content?.elements;
     const btnDetails = content?.buttonType;
     const background = content?.background?.hex;
     const shadow = content?.shadow;
@@ -20,6 +23,10 @@ const Carousel3 = ({ content}: any) => {
     const animationDuration = content?.animation?.animationDuration
     const animationName = content?.animation?.animationName
     const animationOffset = content?.animation?.animationOffset
+
+    console.log(`============Carousel2===============`)
+    console.log(carouselElements)
+    console.log(`============Carousel2=====================`)
 
     return (
         <div className="my-12 p-5 relative mx-5 md:mx-12 bg-white shadow-carouselBox2">
@@ -61,48 +68,27 @@ const Carousel3 = ({ content}: any) => {
             slidesPerView={1}
             spaceBetween={20}
         >
-            {[...Array(4)].map((d, i) => (
-            <SwiperSlide key={i} className="flex flex-col">
+            {carouselElements.map((val: any, i: number) => (
+            <SwiperSlide 
+                key={i} 
+                className="flex flex-col">
                 <div className="shadow-carouselBox2 text-center md:text-left md:space-x-8 p-3 flex space-y-5 md:space-y-0 flex-col md:flex-row items-center">
                 <div className="h-[150px] w-[150px] md:h-[190px] md:w-[190px] lg:w-[208px] lg:h-[208px] flex-shrink-0 overflow-hidden rounded-full">
-                    <Image
-                    src={`/img/girl.png`}
+                <img
+                    src={utilities.ImageFn(val?.Image?.asset?._ref)}
                     alt=""
-                    width="100%"
-                    height="100%"
-                    layout="responsive"
-                    objectFit="cover"
-                    />
+                    className="w-full h-full object-center object-cover"
+                />
                 </div>
                 <div className="flex flex-col space-y-4">
                     <div className="flex flex-col">
-                    <h4 className="text-primary-800 font-semibold text-2xl md:text-3xl lg:text-4xl">
-                        Tom Holland
-                    </h4>
-                    <span className="text-primary-700 text-xs md:text-sm lg:text-xl font-medium">
-                        Executive at ABC Limited
-                    </span>
+                    <div className="mb-4">
+                        <CustomText content={val?.heading?.text} textAlign="center" />
                     </div>
-                    <p className="relative placeholder:text-xs md:text-sm lg:text-xl md:font-light md:leading-[2.1] font-medium leading-loose">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                    irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur.{" "}
-                    <div className="absolute -bottom-3 -right-3 -z-10">
-                        <div className="relative h-10 w-12 md:h-12 md:w-14 lg:h-14 lg:w-16">
-                        <Image
-                            src={`/img/quote.png`}
-                            alt=""
-                            width="100%"
-                            height="100%"
-                            layout="responsive"
-                            objectFit="contain"
-                        />
-                        </div>
+                    <div style={{ zIndex: 4 }}>
+                    <CustomText content={val?.body?.text} textAlign="center"/>
                     </div>
-                    </p>
+                </div>
                 </div>
                 </div>
             </SwiperSlide>
